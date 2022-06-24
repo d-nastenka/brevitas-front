4979d16c2cb8f42f32d0346362baeadcad12157c
d-nastenka <deduhina01@mail.ru>
2022-06-24 11:13:02 +0700

    use for

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <!-- <input v-model="msg">
     <div v-text="msg">  </div> -->
     
-    <div >
+    <!-- <div >
       <div v-if="status"> 
         Условие да
       </div>
@@ -18,7 +18,19 @@
         Условие нет
       </div>
     </div>
-    <button @click="changeStatus"> Сменить условие </button>
+    <button @click="changeStatus"> Сменить условие </button> -->
+
+    <div v-for="(user, key) in users" :key="key">
+      <div>
+        {{ user.name }} 
+        {{ user.age }}
+      </div>
+      <button @click="deleteUser(key)">Delete </button>
+    </div>
+
+    <input v-model="tempUser" >
+    <button @click="addUser">Add</button>
+
   </div>
 
 </template>
@@ -31,13 +43,30 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       status: true,
-      users: ['Jonh', 'Ben', 'Ann']
+      users: [{
+        name: 'John',
+        age: 20,
+      }, {
+        name: 'Ben',
+        age: 21
+      }, {
+        name:'Ann',
+        age: 20
+      }],
+      tempUser: ''
     }
   },
   methods: {
     changeStatus() {
       this.status = !this.status
 
+    },
+    addUser() {
+      this.users.push(this.tempUser);
+      this.tempUser = ''
+    },
+    deleteUser(i) {
+        this.users.splice(i, 1)
     }
   }
   // computed: {
