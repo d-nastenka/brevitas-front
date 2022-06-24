b95309837a6791516d43f282858a83f730bd97c7
d-nastenka <deduhina01@mail.ru>
2022-06-24 12:05:35 +0700

    add form and users components

---
+++ src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+   <form @submit.prevent="submitForm">
+      <div>
+        <input v-model="form.name">
+      </div>
+      <div>
+        <input v-model="form.surname">
+      </div>
+      <div>
+        <input v-model="form.age">
+      </div>
+      <div v-for="(i, key) in form" :key="key">
+        
+        {{ form.name }} 
+        {{ form.surname }}
+        {{ form.age }}
+      </div>
+      <button type="submit"> Create user</button>
+
+    </form>  
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    data () {
+        return {
+            form: [{
+                name: '',
+                surname: '',
+                age: 0
+            }]
+            
+        }
+    },
+    methods: {
+    
+    submitForm () {
+      console.log(this.form)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div v-for="(user, key) in users" :key="key">
+            <div>
+                {{ user.name }} 
+                {{ user.age }}
+            </div>
+        <button @click="deleteUser(key)">Delete </button>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Users',
+    data () {
+        return {
+        
+            users: [{
+                name: 'John',
+                age: 20,
+            }, {
+                name: 'Ben',
+                age: 21
+            }, {
+                name:'Ann',
+                age: 20
+            }],
+            tempUser: '',
+        }
+    },
+    methods: {
+        addUser() {
+            this.users.push(this.tempUser);
+            this.tempUser = ''
+        },
+        deleteUser(i) {
+            this.users.splice(i, 1)
+        }
+  
+    }
+}
+</script>
+
+
+<style >
+
+</style>
