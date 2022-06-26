2781649ee853cf035353a8b6c598318762c1f8bb
sunmantra <zcernyh14@gmail.com>
2022-06-27 01:36:46 +0700

    footer

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,20 @@
     <!-- <component :is="layout">
           <router-view/>
     </component> -->
-
+<footer class="footer">
+        <div class="foot-row">
+            <div class="footkont">
+                   Наше расположение
+                <p class="foot-text">Россия <br> г.Красноярск, улица, дом</p>
+            </div>
+            <div class="footkont">
+                контакты
+             <p class="foot-text">+ 8 (913)-99-99 <br>почта@gmail.com </p>
+         </div>
+        </div>      
+    </footer>
+    
   </div>
-
 </template>
 
 <script>
@@ -120,13 +131,13 @@ a {
 }
 
 .head {
-    width: 102%;
+    width: 101%;
     height: 200px;
     display: flex;
     justify-content: center;
     align-items: center; 
     margin-top: -30px; 
-    margin-left: -5px;
+    margin-left: -7px;
     
 
     background-color: #9FEDD7;
@@ -153,5 +164,35 @@ a {
 .pages_ell{
   margin-left: 25px;
 }
+
+/*подвал*/
+.footer{
+    background: #026670;
+    margin-top: 30px;
+    margin-left: -7px;
+    margin-bottom: -7px;
+    padding-top: 30px;
+    padding-bottom: 20px; 
+    width: 101%;
+    height: 200px;
+    justify-content: center;
+    align-items: center; 
+    
+}
+
+.foot-row{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 1.3;
+}
+.foot-text{
+    font-weight: 300;
+    font-size: 20px;
+    line-height:  1.3; 
+}
+/*конец подвал*/
 
 </style>
