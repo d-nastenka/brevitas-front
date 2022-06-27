223960c234f898d8bfa5fe6ccea3c5a96c2d832b
d-nastenka <deduhina01@mail.ru>
2022-06-27 14:24:04 +0700

    add form cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,35 @@
 <template>
-    <div>
-        <h3>Создать новую визитку</h3>
+    <div class="page-card">
         
+        <div class="form-cards">
+            <div class="title-cards">
+                <h3>Создать новую визитку</h3>
+            </div>
+            <form @submit.prevent="submitForm">
+                <div class="form-input-cards">
+                    <div class="cards-input">
+                        <input v-model="form.name" placeholder="Имя">
+                    </div>
+                    <div class="cards-input">
+                        <input v-model="form.surname" placeholder="Фамилия">
+                    </div>
+                    <div class="cards-input">
+                        <input v-model="form.description" placeholder="Описание">
+                    </div>
+                    <div class="cards-input">
+                        <input v-model="form.email" placeholder="Почта">
+                    </div>
+                    <div class="cards-input">
+                        <input v-model="form.link" placeholder="Ссылка">
+                    </div>
+                    <div class="cards-input">
+                        <input v-model="form.phone" placeholder="Телефон">
+                    </div>
+                    <button type="submit" class="btn-card"> Создать визитку</button>
+                </div>
+            </form>
+
+        </div>
         
     </div>
 </template>
@@ -9,6 +37,71 @@
 <script>
 
 export default {
-    name: "Card"
+    name: "Card",
+    data () {
+        return {
+            form: {
+                name: '',
+                surname: '',
+                description: '',
+                email: '',
+                link: '',
+                phone: ''
+            }
+            
+        }
+    },
 }
 </script>
+
+<style scoped>
+
+.page-card{
+    display: flex;
+    justify-content: center;
+
+    margin-top: 20px;
+}
+
+.form-cards {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    
+
+    width: 300px;
+    padding: 32px;
+    border-radius: 10px;
+    box-shadow: 0 4px 16px #ccc;
+}
+
+.title-cards {
+    text-align: center;
+    margin: 0 0 20px 0;
+    font-weight: normal;
+}
+
+
+
+.form-input-cards {
+    width: 100%;
+    padding: 0 0 10px 0;
+    border: none;    
+}
+
+.btn-card { 
+    background-color: rgba(63, 63, 63, 0.897);
+    color: white;
+    font-size: 14px;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 7px;
+    text-align: center;
+}
+
+.btn-card:hover {
+    background-color: white;
+    color: rgb(76, 76, 76);
+}
+</style>
