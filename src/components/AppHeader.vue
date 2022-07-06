1e8c36132f5c3cb32bd3fa60158d0cc6099ac42d
d-nastenka <deduhina01@mail.ru>
2022-07-06 15:55:54 +0700

    add authorization

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,28 +1,56 @@
 <template>
   <header class="head">
     <div class="pages">
-        <h1 class="head_title">Brevitas</h1>
-        <a class="btn_head" @click="$router.push('/')">
+      <h1 class="head_title">Brevitas</h1>
+      <a class="btn_head" @click="$router.push('/')">
         Главная страница
       </a>
-      <a  v-if="$store.state.isAuth" class="btn_head" @click="$router.push('/card')">
-      Создать новую визитку</a>
-      <a  v-if="$store.state.isAuth" class="btn_head" @click="$router.push('/mycards')"> Мои визитки</a>
-      <div v-else class="btn_head">     
-        <a class="btn_autor" @click="$router.push('/authorization')"> <img class="img_reg" src="/image/reg.png" alt=""> Войти</a>
-
-
+      <a v-if="isAuth" class="btn_head" @click="$router.push('/card')">
+        Создать новую визитку</a
+      >
+      <a v-if="isAuth" class="btn_head" @click="$router.push('/mycards')">
+        Мои визитки</a
+      >
+      <a v-if="isAuth" class="btn_head" @click="usLogout">Выход</a>
+      <div v-else class="btn_head">
+        <a class="btn_autor" @click="$router.push('/authorization')">
+          <img class="img_reg" src="/image/reg.png" alt="" />Войти</a
+        >
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import { mapMutations, mapState } from "vuex";
+
 export default {
   name: "Header",
   mounted() {
     console.log(this.$store);
+  },
+  methods: {
+    async usLogout() {
+      const res = await fetch("http://localhost:3000/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+      this.$router.push("/authorization");
+    },
+    ...mapMutations(['ChangeAuth'])
+
+  },
+  computed: {
+    ...mapState(["isAuth"]),
+    
   }
+  // computed: mapState ({
+  //     //return this.$store.state.isAuth;
+  //     isAuthUs: state => state.isAuth,
+  //   }),
 };
 </script>
 
@@ -49,7 +77,7 @@ export default {
   margin-left: 30px;
 
   font-size: 70px; /*шрифт*/
-  font-family: 'Ubuntu', sans-serif;
+  font-family: "Ubuntu", sans-serif;
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
@@ -57,8 +85,8 @@ export default {
 
   color: #fce4de;
 
-  /*/*бакграунд*/ 
-    /*background-image: linear-gradient(to right,#fce4de, #fcfcfc);
+  /*/*бакграунд*/
+  /*background-image: linear-gradient(to right,#fce4de, #fcfcfc);
     -webkit-background-clip: text;
     background-clip: text;
     color: transparent;*/
